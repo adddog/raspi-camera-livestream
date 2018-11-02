cf08f28b393eda1806c2aef29f6d9828931e8430
samelie <sam@stinkdigital.com>
2018-11-01 23:39:04 -0700

    express

--- server/ffmpeg.js
+++ server/ffmpeg.js
@@ -2,7 +2,7 @@ var spawn = require('child_process').spawn
 var exec = require('child_process').exec
 
 function ffmpeg() {
-  function start() {
+  function start(key) {
     // console.log('here')
     // command = exec(`ffplay udp://192.168.1.81:7777`)
     command = exec(`raspivid -o - -t 0 -vf -hf -fps 30 -b 6000000 | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/${key}`)
--- server/index.js
+++ server/index.js
@@ -1,4 +1,48 @@
+const ffmpeg = require('./ffmpeg')()
+
 const path = require('path')
+
+const express = require('express')
+var bodyParser = require('body-parser')
+
+const app = express()
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+const port = 3000
+
+app.use(express.static(path.join(process.cwd(), 'public')))
+
+app.get('/', (request, reply) => {
+  reply.sendFile('index.html') // serving path.join(__dirname, 'public', 'myHtml.html') directly
+})
+
+app.post('/stream', (req, reply) => {
+  const { body } = req
+  console.log(body)
+  if (!body.key) return { message: 'no key' }
+  switch (body.action) {
+    case 'start':
+      {
+        ffmpeg.start(body.key)
+      }
+      break
+    case 'stop':
+      {
+        ffmpeg.stop()
+      }
+      break
+  }
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+/*
+
+
+
+
+
 const fastify = require('fastify')()
 
 const ffmpeg = require('./ffmpeg')()
@@ -28,6 +72,8 @@ fastify.post('/stream', async ({ query, params, headers, body }, reply) => {
 })
 
 fastify.listen(3000, (err, address) => {
+  console.log(`server listening on ${address}`);
   if (err) throw err
   fastify.log.info(`server listening on ${address}`)
 })
+*/
